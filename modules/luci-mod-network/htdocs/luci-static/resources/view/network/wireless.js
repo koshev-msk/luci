7d993197f2ed948fe93df824fc4eb867528cff7f
appleneko2001 <appleneko2001@outlook.com>
2024-12-27 21:37:27 +0900

    luci-mod-network: add QRCode button and modal functions to wireless.js

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -6,6 +6,7 @@
 'require ui';
 'require rpc';
 'require uci';
+'require uqr';
 'require form';
 'require network';
 'require firewall';
@@ -968,6 +969,11 @@ return view.extend({
 
 				btns = [
 					E('button', {
+						'class': 'cbi-button cbi-button-neutral',
+						'title': _('QR Code'),
+						'click': ui.createHandlerFn(this, 'handleShareCode', section_id)
+					}, _('QR Code')),
+					E('button', {
 						'class': 'cbi-button cbi-button-neutral enable-disable',
 						'title': isDisabled ? _('Enable this network') : _('Disable this network'),
 						'click': ui.createHandlerFn(this, network_updown, section_id, this.map)
@@ -986,6 +992,71 @@ return view.extend({
 			}
 
 			return E('td', { 'class': 'td middle cbi-section-actions' }, E('div', btns));
+		};
+
+		s.handleShareCode = function (id, ev) {
+			if(uci.get('wireless', id, 'mode') !== 'ap')
+				return null;
+
+			var ssid = uci.get('wireless', id, 'ssid');
+
+			var encryption = uci.get('wireless', id, 'encryption');
+			var haveKey = true;
+
+			if(encryption == 'none' || encryption == 'owe')
+			{
+				encryption = 'nopass';
+				haveKey = false;
+			}
+			else if(encryption == 'psk') { encryption = 'wep' }
+			else if(encryption == 'psk2' || encryption == 'psk2+ccmp' || encryption == 'psk2+tkip' || encryption == 'psk2+tkip+ccmp')
+			{ encryption = 'wpa' }
+
+			var qrstring = 'WIFI:S:'+ escape_string(ssid) +';T:' + encryption + ';'
+			if(haveKey)
+				qrstring = qrstring + 'P:' + escape_string(uci.get('wireless', id, 'key')) + ';';
+
+			if(uci.get('wireless', id, 'hidden') == '1')
+				qrstring = qrstring + 'H:true;'
+
+			const options = {
+				pixelSize: 4,
+				whiteColor: 'white',
+				blackColor: 'black'
+			};
+			const svg = uqr.renderSVG(qrstring, options);
+
+			var md = ui.showModal(_('QR Code'), [
+				E('div', { 'class': 'center' }, [
+					E('div', {}, [
+						E('span', {}, _('SSID')),
+						': ',
+						E('span', {}, ssid)]
+					),
+					toElem(svg)
+				]),
+				E('div', { 'class': 'center' }, [
+					E('button', {
+						'class': 'btn',
+						'click': L.bind(this.handleShareCodeDismiss, this)
+					}, _('Dismiss'))
+				])
+			]);
+
+			md.style.maxWidth = '20%';
+			md.style.maxHeight = 'none';
+
+			return E(svg);
+		}
+
+		s.handleShareCodeDismiss = function(ev) {
+			var md = dom.parent(ev.target, 'div[aria-modal="true"]');
+			if (md) {
+				md.style.maxWidth = '';
+				md.style.maxHeight = '';
+			}
+
+			ui.hideModal();
 		};
 
 		s.addModalOptions = function(s) {
