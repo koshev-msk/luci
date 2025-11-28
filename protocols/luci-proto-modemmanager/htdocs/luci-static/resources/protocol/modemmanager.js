bb0e34919c2df03957afb46c6bf60d77ae937972
Konstantine Shevlakov <66990040+koshev-msk@users.noreply.github.com>
2025-11-28 14:18:42 +0300

    Update modemmanager.js

--- protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
+++ protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
@@ -141,7 +141,7 @@ return network.registerProtocol('modemmanager', {
 		o.value('ipv6', _('IPv6 only'));
 		o.default = 'ipv4v6';
 
-		o = s.taboption('general', form.Flag, 'allow_roaming', _('Deny Roaming'), _('Deny conntect to roaming Networks'));
+		o = s.taboption('general', form.Flag, 'allow_roaming', _('Deny Roaming'), _('Deny connect to roaming Networks'));
 		o.enabled = '0';
 
 		o = s.taboption('advanced', form.Value, 'mtu', _('Override MTU'));
