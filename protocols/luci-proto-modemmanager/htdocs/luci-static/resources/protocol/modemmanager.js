85b5bd8c551f9af41ebbc10dee3584c67f98078c
Konstantine Shevlakov <66990040+koshev-msk@users.noreply.github.com>
2025-11-28 14:17:48 +0300

    luci-proto-modemmanager: Invert Roaming flag

--- protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
+++ protocols/luci-proto-modemmanager/htdocs/luci-static/resources/protocol/modemmanager.js
@@ -141,7 +141,8 @@ return network.registerProtocol('modemmanager', {
 		o.value('ipv6', _('IPv6 only'));
 		o.default = 'ipv4v6';
 
-		o = s.taboption('general', form.Flag, 'allow_roaming', _('Allow Roaming'), _('Allow conntect to roaming Networks'));
+		o = s.taboption('general', form.Flag, 'allow_roaming', _('Deny Roaming'), _('Deny conntect to roaming Networks'));
+		o.enabled = '0';
 
 		o = s.taboption('advanced', form.Value, 'mtu', _('Override MTU'));
 		o.placeholder = dev ? (dev.getMTU() || '1500') : '1500';
